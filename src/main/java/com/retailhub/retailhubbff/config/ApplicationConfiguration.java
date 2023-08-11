5f45ad136e9c10ddd15796ec5f95118175904d5c
rameshBabuOnGit <rameshbabu.b@brillio.com>
2023-08-11 12:16:45 +0530

    Added dependency for spring security config Added configuration for oauth2 login using google and github

--- src/main/java/com/retailhub/retailhubbff/config/ApplicationConfiguration.java
+++ src/main/java/com/retailhub/retailhubbff/config/ApplicationConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.client.RestTemplate;
 
-@Import({RestTemplate.class})
+@Import({RestTemplate.class, SecurityConfig.class})
 @Configuration
 public class ApplicationConfiguration {
 }
